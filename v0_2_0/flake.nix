5fda123f31b3c5fd744cc59a3a6f2ac42bd5f417
hugosenari <hugosenari@gmail.com>
2022-07-16 20:53:39 -0300

    chore: re index statistical_tests

--- v0_2_0/flake.nix
+++ v0_2_0/flake.nix
@@ -7,11 +7,11 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs.src-statistical_tests-v0_1_0.flake = false;
-  inputs.src-statistical_tests-v0_1_0.ref   = "refs/tags/v0.1.0";
-  inputs.src-statistical_tests-v0_1_0.owner = "ayman-albaz";
-  inputs.src-statistical_tests-v0_1_0.repo  = "statistical-tests";
-  inputs.src-statistical_tests-v0_1_0.type  = "github";
+  inputs.src-statistical_tests-v0_2_0.flake = false;
+  inputs.src-statistical_tests-v0_2_0.ref   = "refs/tags/v0.2.0";
+  inputs.src-statistical_tests-v0_2_0.owner = "ayman-albaz";
+  inputs.src-statistical_tests-v0_2_0.repo  = "statistical-tests";
+  inputs.src-statistical_tests-v0_2_0.type  = "github";
   
   inputs."distributions".owner = "nim-nix-pkgs";
   inputs."distributions".ref   = "master";
@@ -24,13 +24,13 @@
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
   let 
     lib  = flakeNimbleLib.lib;
-    args = ["self" "nixpkgs" "flakeNimbleLib" "src-statistical_tests-v0_1_0"];
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-statistical_tests-v0_2_0"];
     over = if builtins.pathExists ./override.nix 
            then { override = import ./override.nix; }
            else { };
   in lib.mkRefOutput (over // {
     inherit self nixpkgs ;
-    src  = deps."src-statistical_tests-v0_1_0";
+    src  = deps."src-statistical_tests-v0_2_0";
     deps = builtins.removeAttrs deps args;
     meta = builtins.fromJSON (builtins.readFile ./meta.json);
   } );
